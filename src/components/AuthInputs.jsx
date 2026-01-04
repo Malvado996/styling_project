6bbb21340bda70a59d21cfea9acc178abe5c4c7d
Philip Rodrigues <philip.rodrigues.js@gmail.com>
2026-01-03 22:00:02 -0500

    nested rules for styled components

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -27,6 +27,20 @@ const Input = styled.input`
   border-radius: 0.25rem;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
+`
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -71,10 +85,10 @@ export default function AuthInputs() {
         </p>
       </ControlContainer>
       <div className="actions">
-        <button type="button" className="text-button">
+        <Button type="Button" className="text-Button">
           Create a new account
-        </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        </Button>
+        <Button className='Button' onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
